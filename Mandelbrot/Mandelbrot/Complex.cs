fbde7bf4e8e7ddc82b0b43c3013344f9e554a6b9
Arthur Liss <artli@e1.ru>
2014-11-22 15:49:58 +0500

    First iteration

---
+++ Mandelbrot/Mandelbrot/Complex.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandelbrot {
+    public class Complex : IEquatable<Complex> {
+        public double X, Y;
+
+        public Complex(double x, double y) {
+            X = x;
+            Y = y;
+        }
+
+        public static Complex ByAngle(double angle, double length) {
+            return new Complex(Math.Cos(angle) * length, Math.Sin(angle) * length);
+        }
+
+        public double Length() {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+
+        public double Angle() {
+            var angle = Math.Acos(X / Length());
+            if (Y < 0)
+                angle = -angle;
+            return angle;
+        }
+
+        public static Complex operator +(Complex a, Complex b) {
+            return new Complex(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Complex operator -(Complex a, Complex b) {
+            return new Complex(a.X - b.X, a.Y - b.Y);
+        }
+
+        public static Complex operator *(Complex a, int k) {
+            return new Complex(a.X * k, a.Y * k);
+        }
+
+        public static Complex operator *(Complex a, Complex b) {
+            return new Complex(a.X * b.X - a.Y * b.Y, a.X * b.Y + a.Y * b.X);
+        }
+
+        public override string ToString() {
+            return "(" + X.ToString() + ", " + Y.ToString() + ")";
+        }
+
+        public static bool DoublesEqual(double d1, double d2) {
+            return Math.Abs(d1 - d2) < 1e-6;
+        }
+
+        public bool Equals(Complex that) {
+            return DoublesEqual(X, that.X) && DoublesEqual(Y, that.Y);
+        }
+    }
+}
---
+++ Mandelbrot/Mandelbrot/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Mandelbrot {
+    static class Generator {
+        const int MaxIterations = 200;
+
+        static bool TestPoint(Complex c) {
+            Complex z = c;
+            for (int i = 0; i < MaxIterations; i++) {
+                z = z*z + c;
+                if (z.Length() > 2)
+                    return true;
+            }
+            return false;
+        }
+        
+        public static bool[][] Generate(double minx, double maxx, double miny, double maxy, int width, int height) {
+            var result = new bool[width][];
+            foreach (var i in Enumerable.Range(0, width)) {
+                result[i] = new bool[height];
+            }
+
+            var stepX = (maxx - minx) / width;
+            var stepY = (maxy - miny) / height;
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    result[i][j] = TestPoint(new Complex(minx + i*stepX, miny + j*stepY));
+
+            return result;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            double minx = 0;
+            double maxx = 2;
+            double miny = -1;
+            double maxy = 1;
+            int width = 200;
+            int height = 200;
+
+            var lines = new List<string>();
+            var result = Generator.Generate(minx, maxx, miny, maxy, width, height);
+            for (int j = height - 1; j >= 0; j--) {
+                var line = new List<char>();
+                for (int i = 0; i < width; i++) {
+                    if (result[i][j])
+                        line.Add('.');
+                    else
+                        line.Add('@');
+                }
+                lines.Add(string.Join("", line));
+            }
+
+            File.WriteAllLines("output.txt", lines);
+        }
+    }
+}
